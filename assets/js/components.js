f23e7b6f4d94626b9fd7ad1a7206b6fa3f68cbbe
devjasonlin <linmjnz@gmail.com>
2025-04-18 17:12:37 +1200

    tweak projects section

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,6 +1,10 @@
 import { MarqueeEle } from './components/marquee-ele.js';
-
+import { ProjectCard } from './components/project-card.js';
 
 if (!window.customElements.get('marquee-ele')) {
   customElements.define('marquee-ele', MarqueeEle);
+}
+
+if (!window.customElements.get('project-card')) {
+  customElements.define('project-card', ProjectCard);
 }
---
+++ assets/js/components/project-card.js
@@ -0,0 +1,27 @@
+
+
+
+export class ProjectCard extends HTMLElement {
+  constructor() {
+    super();
+    this.projectCardInit();
+  }
+  projectCardInit() {
+    this.addEventListener('mousemove', function(e) {
+      const x = e.clientX - this.getBoundingClientRect().left;
+      const y = e.clientY - this.getBoundingClientRect().top;
+
+      const centerX = this.offsetWidth / 2;
+      const centerY = this.offsetHeight / 2;
+      
+      const angleX = (y - centerY) / 12;
+      const angleY = (centerX - x) / 12;
+      
+      this.style.transform = `perspective(1000px) rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+    });
+    
+    this.addEventListener('mouseleave', function() {
+        this.style.transform = 'perspective(1000px) rotateX(0) rotateY(0)';
+    });
+  }
+}
