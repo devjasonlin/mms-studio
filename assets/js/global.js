24684b79c3cb194e1bc39ffe36fca86481702325
devjasonlin <linmjnz@gmail.com>
2025-04-20 21:48:05 +1200

    implement animation

---
+++ assets/js/global.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".visible-animation");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        const item = entry.target;
+
+        // 添加延迟，基于当前进入视图的卡片在 NodeList 中的顺序
+        const delay = Array.from(items).indexOf(item) * 150;
+
+        setTimeout(() => {
+          item.classList.add("visible");
+        }, delay);
+
+        observer.unobserve(item); // 确保只触发一次
+      }
+    });
+  }, {
+    threshold: 0.05 // 卡片露出10%就触发动画
+  });
+
+  items.forEach(item => observer.observe(item));
+});
